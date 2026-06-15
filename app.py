d6251409d79d7b7a83200d53124bb1566e7f5371
amarpal <119533587+amarpal-singh5@users.noreply.github.com>
2026-06-15 10:49:49 -0500

    Convert app to Flask service

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
-def add(a, b):
-    return a + b
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Kubernetes!"
+
+@app.route("/health")
+def health():
+    return "OK"
 
 if __name__ == "__main__":
-    print(add(2, 3))
+    app.run(host="0.0.0.0", port=5000)
